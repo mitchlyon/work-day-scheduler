49323522f487c0d4242e7c4864723c37e3278be1
Mitchell Lyon <kekoa.lyon@gmail.com>
2021-08-29 19:20:59 -0600

    added textarea element

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 // set date 
 var dateEl = document.getElementById('datetime');
 
-var updateDate = function() {
+var updateDate = function () {
     var currentDate = moment();
-    var displayDate = currentDate.format("MM/DD/YYYY, hh:mm");
+    var displayDate = currentDate.format("MMM/DD/YYYY, hh:mm");
 
     dateEl.textContent = displayDate;
 }
@@ -13,55 +13,56 @@ setInterval(() => {
 });
 
 
-var tasks = document.getElementById("#tasks")
-
-var loadTasks = function () {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-}
-
-//add click method to add tasks
-$(".list-group").on("click", "p", function() {
-    var text = $(this)
-    .text()
-    .trim();
-
-    var textInput = $("<textarea>")
-    .addClass("form-control")
-    .val(text);
-
-    $(this).replaceWith(textInput);
-
-    textInput.trigger("focus");
-});
-
-$("list-group").on("blur", "textarea", function() {
-    var text = 4(this)
-    .val()
-    .trim()
-
-    var taskP = $("<p>")
-    
-    .text(text);
-
-    $(this).replaceWith(taskP)
-
-    saveTasks()
-})
 // save tasks
-var saveTasks = function () {
-    localStorage.setItem("tasks", tasks)
-    console.log(localStorage)
-  };
+$(document).ready(function () {
+   
+    $(".saveBtn").on("click", function () {
+       
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-document.getElementById("btn").addEventListener("click", function() {
-    saveTasks()
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    });
     
+    function timeTracker() {
+        
+        var timeNow = moment().hour();
+
+        
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+    
+    //load task
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4 .description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
+
+    timeTracker();
 });
-
-// load tasks
-
-
-
-
-
-
